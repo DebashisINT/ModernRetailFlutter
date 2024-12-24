0e33d63bbfb6e5b5426ee095b300d51513868b18
SumanBacharIndusNet <suman.bachar@indusnet.co.in>
2024-12-24 12:17:14 +0530

    c

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -13,8 +13,6 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreen extends State<DashboardScreen> {
-
-
   @override
   void initState() {
     super.initState();
@@ -22,139 +20,84 @@ class _DashboardScreen extends State<DashboardScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Dashboard'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          children: [
-            // Left container (Stores)
-            Expanded(
-              child: Container(
-                child: Stack(
-                  alignment: Alignment.center,  // Align the image at the center
-                  children: [
-                    // CardView for the text "Stores" slightly down
-                    Positioned(
-                      top: 20, // Slightly push the CardView down
-                      child: Container(
-                        width: 175,  // CardView width
-                        height: 95, // CardView height
-                        decoration: BoxDecoration(
-                          color: Color(0xEBEFE0),
-                          borderRadius: BorderRadius.circular(10),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.2), // Shadow color
-                              blurRadius: 0, // Soft shadow
-                              offset: Offset(0, 0), // Shadow offset
-                            ),
-                          ],
-                        ),
-                        child: Center(
-                          child: Text(
-                            "Stores",
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ),
-                    ),
-                    // Circular ImageView at the top center
-                    Positioned(
-                      top: 0, // Position it at the top
-                      child: Container(
-                        width: 50, // Diameter of the circle (fixed size)
-                        height: 50,
-                        decoration: BoxDecoration(
-                          color: Color(0xFF88BFFF), // Background color
-                          shape: BoxShape.circle, // Circle shape
-                        ),
-                        child: Center( // This will center the image inside the circle
-                          child: Image.asset(
-                            'assets/images/ic_store_color.jpg', // Your image file
-                            width: 30, // You can set the size of the image (optional)
-                            height: 30, // You can set the size of the image (optional)
-                            fit: BoxFit.contain, // Keeps the aspect ratio intact and ensures the image fits within the circle
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Dashboard')),
+        body: Padding(
+          padding: const EdgeInsets.all(6.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(height: 30),
+              // First Row
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  buildCustomCard(Icons.store, 'Stores'),
+                  buildCustomCard(Icons.inventory, 'Stock'),
+                ],
               ),
-            ),
-            SizedBox(width: 10), // Space between the two containers
-            // Right container (Stock)
-            Expanded(
-              child: Container(
-                child: Stack(
-                  alignment: Alignment.center,  // Align the image at the center
-                  children: [
-                    // CardView for the text "Stock" slightly down
-                    Positioned(
-                      top: 20, // Slightly push the CardView down
-                      child: Container(
-                        width: 175,  // CardView width
-                        height: 95, // CardView height
-                        decoration: BoxDecoration(
-                          color: Color(0xEBEFE0), // Card background color (Light grey)
-                          borderRadius: BorderRadius.circular(10), // Rounded corners
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.2), // Shadow color
-                              blurRadius: 0, // Soft shadow
-                              offset: Offset(0, 0), // Shadow offset
-                            ),
-                          ],
-                        ),
-                        child: Center(
-                          child: Text(
-                            "Stock",
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ),
-                    ),
-                    // Circular ImageView at the top center
-                    Positioned(
-                      top: 0, // Position it at the top
-                      child: Container(
-                        width: 50, // Diameter of the circle (fixed size)
-                        height: 50,
-                        decoration: BoxDecoration(
-                          color: Color(0xFF88BFFF), // Background color (Blue)
-                          shape: BoxShape.circle, // Circle shape
-                        ),
-                        child: Center( // This will center the image inside the circle
-                          child: Image.asset(
-                            'assets/images/ic_stock.jpg', // Your image file
-                            width: 30, // Image width inside the circle
-                            height: 30, // Image height inside the circle
-                            fit: BoxFit.contain, // Ensures the image fits inside the circle
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+              SizedBox(height: 30),
+
+              // Second Row
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  buildCustomCard(Icons.grid_on, 'Planogram'),
+                  buildCustomCard(Icons.feedback, 'Complaint & Feedback'),
+                ],
               ),
-            ),
-          ],
+              SizedBox(height: 30),
+
+            ],
+          ),
         ),
       ),
-
     );
   }
+
+  Widget buildCustomCard(IconData icon, String title) {
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        // Background card
+        Container(
+          width: 150,
+          height: 100,
+          decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Center(
+            child: Text(
+              'Stores',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ),
+        // Positioned circle icon
+        Positioned(
+          top: -25,
+          left: 50,
+          child: CircleAvatar(
+            radius: 25,
+            backgroundColor: Colors.blue,
+            child: Icon(
+              Icons.store, // Use a store-related icon
+              size: 30,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
 }
 
 
