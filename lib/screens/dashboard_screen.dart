36f638d663b917701070b0ae9960f83a94ed533d
Pujabasak <puja.basak@indusnet.co.in>
2024-12-26 11:47:42 +0530

    c dashboard design update

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,187 +1,275 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import '../app_color.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_demo_one/app_color.dart';
 
-
-class DashboardScreen extends StatefulWidget {
-  const DashboardScreen({super.key});
-
-  State<DashboardScreen> createState() => _DashboardScreen();
-
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: DashboardScreen(),
+  ));
 }
 
-class _DashboardScreen extends State<DashboardScreen> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
+class DashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text(
-            'Dashboard',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 17,// Set the text color to white
-            ),
-          ),
-        ),
-        backgroundColor: AppColor.colorToolbar, // Custom toolbar color
-        foregroundColor: Colors.white, // Set icon color to white (for any icons in the AppBar)
-        actions: [
-          IconButton(
-            icon: Icon(Icons.notifications), // The notification icon
-            onPressed: () {
-              print('Notification icon pressed');
-            },
-          ),
-        ],
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        //statusBarColor: Colors.white, // Set status bar color to white
+        statusBarIconBrightness: Brightness.light, // Set icons to dark (black)
       ),
-
+    );
+    return Scaffold(
       drawer: Drawer(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
         child: ListView(
           padding: EdgeInsets.zero,
           children: [
-            UserAccountsDrawerHeader(
-              accountName: Text('Puja Basak'),
-              accountEmail: Text('puja@indusnet.co.in'),
-              currentAccountPicture: CircleAvatar(
-                backgroundColor: Colors.white,
-                child: Icon(Icons.person, color: AppColor.colorToolbar, size: 50),
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: AppColor.colorToolbar,
               ),
-              decoration: BoxDecoration(color: AppColor.colorToolbar),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Colors.white,
+                    child: Icon(Icons.person, size: 40, color: Colors.blue),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "User Name",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    "user@example.com",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.white70,
+                    ),
+                  ),
+                ],
+              ),
             ),
-            drawerMenuItem(
-              imagePath: 'assets/images/home.png',
-              title: 'Home',
-              onTap: () => Navigator.pop(context),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+              onTap: () {
+                // Navigate to home or perform actions
+                Navigator.pop(context);
+              },
             ),
-            drawerMenuItem(
-              imagePath: 'assets/images/store.png',
-              title: 'Store',
-              onTap: () => Navigator.pop(context),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+              onTap: () {
+                // Navigate to settings or perform actions
+                Navigator.pop(context);
+              },
             ),
-            drawerMenuItem(
-              imagePath: 'assets/images/stock.png',
-              title: 'Stock',
-              onTap: () => Navigator.pop(context),
-            ),
-            drawerMenuItem(
-              imagePath: 'assets/images/log_out.png',
-              title: 'Logout',
-              onTap: () => Navigator.pop(context),
+            ListTile(
+              leading: Icon(Icons.logout),
+              title: Text('Logout'),
+              onTap: () {
+                // Perform logout actions
+                Navigator.pop(context);
+              },
             ),
           ],
         ),
       ),
-
-      body: Container(
-        margin: EdgeInsets.only(top: 10.0), // Add top margin here
-        padding: const EdgeInsets.only(left: 16.0, right: 16.0), // Adding top margin and horizontal padding
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              SizedBox(height: 35),
-              // First Row
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  buildCustomCard('assets/images/ic_store_color.jpg', 'Stores', Color(0xff88bfff), 'assets/images/dashboard_img_1.jpg'),
-                  buildCustomCard('assets/images/ic_stock.jpg', 'Stock', Color(0xffe79bfe), 'assets/images/dashboard_img_2.jpg'),
-                ],
+      body: Column(
+        children: [
+          buildTopLayout(),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Container(
+                margin: EdgeInsets.only(top: 10.0),
+                padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    SizedBox(height: 35),
+                    buildRow(context, [
+                      buildCustomCard(context, 'assets/images/ic_store_color.jpg', 'Stores', Color(0xff88bfff), 'assets/images/dashboard_img_1.jpg'),
+                      buildCustomCard(context, 'assets/images/ic_stock.jpg', 'Stock', Color(0xffe79bfe), 'assets/images/dashboard_img_2.jpg'),
+                    ]),
+                    SizedBox(height: 35),
+                    buildRow(context, [
+                      buildCustomCard(context, 'assets/images/ic_planogram.jpg', 'Planogram', Color(0xffc1ea87), 'assets/images/dashboard_img_3.jpg'),
+                      buildCustomCard(context, 'assets/images/ic_complaint.jpg', 'Complaint & Feedback', Color(0xffe1d683), 'assets/images/dashboard_img_4.jpg'),
+                    ]),
+                    SizedBox(height: 35),
+                    buildRow(context, [
+                      buildCustomCard(context, 'assets/images/ic_order_color.jpg', 'Order', Color(0xff79dbb5), 'assets/images/dashboard_img_5.jpg'),
+                      buildCustomCard(context, 'assets/images/ic_survey_color.jpg', 'Survey', Color(0xfffdccbc), 'assets/images/dashboard_img_6.jpg'),
+                    ]),
+                    SizedBox(height: 35),
+                    buildRow(context, [
+                      buildCustomCard(context, 'assets/images/ic_merchandise.jpg', 'Merchandising Operations', Color(0xfffeda77), 'assets/images/dashboard_img_7.jpg'),
+                      buildCustomCard(context, 'assets/images/ic_calender.jpg', 'Daily Activity', Color(0xffd3b2fe), 'assets/images/dashboard_img_8.jpg'),
+                    ]),
+                    SizedBox(height: 35),
+                    buildRow(context, [
+                      buildCustomCard(context, 'assets/images/ic_catalog.jpg', 'Product Catalog', Color(0xfff887d7), 'assets/images/dashboard_img_9.jpg'),
+                      buildCustomCard(context, 'assets/images/ic_sales_promotion.jpg', 'Sales Promotion/Offer/Scheme/Discount', Color(0xff93db75), 'assets/images/dashboard_img_10.jpg'),
+                    ]),
+                    SizedBox(height: 35),
+                  ],
+                ),
               ),
-              SizedBox(height: 35),
-              // Second Row
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  buildCustomCard('assets/images/ic_planogram.jpg', 'Planogram', Color(0xffc1ea87), 'assets/images/dashboard_img_3.jpg'),
-                  buildCustomCard('assets/images/ic_complaint.jpg', 'Complaint & Feedback', Color(0xffe1d683), 'assets/images/dashboard_img_4.jpg'),
-                ],
-              ),
-              SizedBox(height: 35),
-              // Third Row
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  buildCustomCard('assets/images/ic_order_color.jpg', 'Order', Color(0xff79dbb5), 'assets/images/dashboard_img_5.jpg'),
-                  buildCustomCard('assets/images/ic_survey_color.jpg', 'Survey', Color(0xfffdccbc), 'assets/images/dashboard_img_6.jpg'),
-                ],
-              ),
-              SizedBox(height: 35),
-              // Fourth Row
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  buildCustomCard('assets/images/ic_merchandise.jpg', 'Merchandising Operations', Color(0xfffeda77), 'assets/images/dashboard_img_7.jpg'),
-                  buildCustomCard('assets/images/ic_calender.jpg', 'Daily Activity', Color(0xffd3b2fe), 'assets/images/dashboard_img_8.jpg'),
-                ],
-              ),
-              SizedBox(height: 35),
-              // Fifth Row
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  buildCustomCard('assets/images/ic_catalog.jpg', 'Product Catalog', Color(0xfff887d7), 'assets/images/dashboard_img_9.jpg'),
-                  buildCustomCard('assets/images/ic_sales_promotion.jpg', 'Sales Promotion/Offer/Scheme/Discount', Color(0xff93db75), 'assets/images/dashboard_img_10.jpg'),
-                ],
-              ),
-              SizedBox(height: 35),
-            ],
+            ),
           ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildTopLayout() {
+    return Container(
+      width: double.infinity,
+      height: 180,
+      decoration: BoxDecoration(
+        color: AppColor.colorToolbar,
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(50),
+          bottomRight: Radius.circular(50),
+        ),
+
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Top Bar with Menu Icon and Bell Icon
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Builder(
+                  builder: (context) => IconButton(
+                    icon: Icon(Icons.menu, color: Colors.white, size: 28),
+                    onPressed: () {
+                      Scaffold.of(context).openDrawer(); // Opens the drawer
+                    },
+                  ),
+                ),
+                Icon(Icons.notifications, color: Colors.white, size: 28),
+              ],
+            ),
+            SizedBox(height: 20),
+            // Welcome Text and Username
+            Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(left: 35.0), // Adds left margin (20dp)
+                  width: 60, // Width of the rectangle
+                  height: 60, // Height of the rectangle
+                  decoration: BoxDecoration(
+                    color: Colors.white, // Background color of the rectangle
+                    borderRadius: BorderRadius.circular(10), // Slightly rounded corners
+                  ),
+                  child: Center(
+                    child: Icon(
+                      Icons.person,
+                      size: 40,
+                      color: AppColor.colorToolbar,
+                    ),
+                  ),
+                ),
+                SizedBox(width: 20),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Welcome,",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      "User Name",
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
   }
 
-  Widget buildCustomCard(String icon, String title, Color backgroundColor, String imagePath) {
+
+  Widget buildRow(BuildContext context, List<Widget> children) {
+    return Row(
+      children: children
+          .map(
+            (child) => Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0), // Space between cards
+            child: child,
+          ),
+        ),
+      )
+          .toList(),
+    );
+  }
+
+  Widget buildCustomCard(BuildContext context, String icon, String title, Color backgroundColor, String imagePath) {
+    double cardWidth = MediaQuery.of(context).size.width * 0.45; // Ensures dynamic width
     return Stack(
-      clipBehavior: Clip.none, // Allows the icon to overflow the card boundaries
+      clipBehavior: Clip.none,
       children: [
         // Background card with image
         Container(
-          width: MediaQuery.of(context).size.width * 0.45,
+          width: cardWidth,
           height: 90,
           decoration: BoxDecoration(
-            color: Colors.grey[200], // Fallback color in case image is not loaded
+            color: Colors.grey[200],
             borderRadius: BorderRadius.circular(10),
             image: DecorationImage(
-              image: AssetImage(imagePath), // Dynamically set background image
-              fit: BoxFit.cover, // Adjust how the image fits inside the card
+              image: AssetImage(imagePath),
+              fit: BoxFit.cover,
             ),
           ),
           child: Center(
             child: Text(
               title,
-              textAlign: TextAlign.center, // Ensures the text is horizontally centered
+              textAlign: TextAlign.center,
               style: TextStyle(
                 fontSize: 14,
                 fontWeight: FontWeight.bold,
-                color: Colors.white, // Set text color to white to make it visible on dark backgrounds
+                color: Colors.white,
               ),
             ),
           ),
         ),
         // Positioned circle icon
         Positioned(
-          top: -25, // Moves the icon upwards, outside the card
-          left: (170 / 2) - 25, // Dynamically centers the icon horizontally
+          top: -25,
+          left: (cardWidth / 2) - 25,
           child: CircleAvatar(
             radius: 25,
-            backgroundColor: backgroundColor, // Dynamic background color
+            backgroundColor: backgroundColor,
             child: ClipOval(
               child: Padding(
-                padding: EdgeInsets.all(10), // Adjust padding as needed
+                padding: EdgeInsets.all(10),
                 child: Image.asset(
                   icon,
-                  fit: BoxFit.cover, // Ensures the image fits within the circle
-                  width: 45,  // Set the width of the image smaller
-                  height: 45, // Set the height of the image smaller
+                  fit: BoxFit.cover,
+                  width: 45,
+                  height: 45,
                 ),
               ),
             ),
@@ -190,33 +278,4 @@ class _DashboardScreen extends State<DashboardScreen> {
       ],
     );
   }
-
-  Widget drawerMenuItem({
-    required String imagePath,
-    required String title,
-    required VoidCallback onTap,
-  }) {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border.all(color: Colors.grey), // Border color for the rectangle
-        borderRadius: BorderRadius.circular(8.0), // Optional for slightly rounded corners
-      ),
-      child: ListTile(
-        leading: Image.asset(
-          imagePath,
-          width: 30,
-          height: 30,
-          fit: BoxFit.cover,
-        ),
-        title: Text(title),
-        visualDensity: VisualDensity.compact,
-        onTap: onTap,
-      ),
-    );
-  }
-
 }
-
-
